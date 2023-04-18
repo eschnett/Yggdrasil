e8859d295a2d64f752fae23f29b9d987dd4c0bcc
Mark Kittisopikul <markkitt@gmail.com>
2023-04-18 16:40:14 -0400

    General target for header

--- H/HDF5/build_tarballs.jl
+++ H/HDF5/build_tarballs.jl
@@ -23,7 +23,7 @@ echo proc_family: ${proc_family}
 echo target: ${target}
 
 if [[ "${target}" == *-mingw* ]]; then
-    cp ${WORKSPACE}/srcdir/headers/pthread_time.h /opt/x86_64-w64-mingw32/x86_64-w64-mingw32/sys-root/include/pthread_time.h
+    cp ${WORKSPACE}/srcdir/headers/pthread_time.h "/opt/${target}/${target}/sys-root/include/pthread_time.h"
     atomic_patch -p1 ${WORKSPACE}/srcdir/patches/h5ls.c.patch
     atomic_patch -p1 ${WORKSPACE}/srcdir/patches/mkdir.patch
     atomic_patch -p1 ${WORKSPACE}/srcdir/patches/strncpy.patch
